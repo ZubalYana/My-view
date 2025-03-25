f29470986c1af30d54d89084dbe8cdba3c2adb29
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-03-25 06:48:17 +0200

    set the basic fronend settings for the auth layout screen

--- src/components/AuthComponents/AuthLayout.jsx
+++ src/components/AuthComponents/AuthLayout.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import WhiteLogo from '../WhiteLogo/WhiteLogo'
+import bgCircle from '/bgCircle.svg'
 export default function AuthLayout() {
   return (
-    <div>
-        <WhiteLogo />
+    <div className='w-full h-screen relative'>
+        <div className='w-full h-screen absolute z-50 px-[60px] py-[40px]'>
+          <WhiteLogo />
+        </div>
+        <div className="w-full h-screen bgCirclesCon absolute z-0 top-0">
+            <img className='bgCircle w-[409px] left-[-80px] top-[-100px]' src={bgCircle} alt="bgCircle" />
+            <img className='bgCircle w-[262px] left-[260px] top-[-80px]' src={bgCircle} alt="bgCircle" />
+            <img className='bgCircle w-[262px] left-[260px] top-[-80px]' src={bgCircle} alt="bgCircle" />
+        </div>
     </div>
   )
 }
--- src/components/WhiteLogo/WhiteLogo.jsx
+++ src/components/WhiteLogo/WhiteLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import whiteLogoEye from '/whiteLogo.svg'
 export default function WhiteLogo() {
   return (
-    <div className='w-[165px] flex justify-between items-center'>
+    <div className='w-[165px] flex justify-between items-center z-10'>
         <img src={whiteLogoEye} alt="whiteLogoEye" />
         <h1 className="text-2xl text-[var(--custom-white)] font-bold">My View</h1>
     </div>
