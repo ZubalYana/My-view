e0517508de8e10be3f07792f426fe6377fac0ff2
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-03-26 06:29:07 +0200

    display homepage with the sidebar layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import AuthLayout from './components/AuthComponents/AuthLayout'
+import LayoutWithSidebar from './components/LayoutWithSidebar/LayoutWithSidebar';
+import Homepage from './components/Homepage/Homepage';
 function App() {
 
   return (
     <Provider store={store}>
       <Router>
         <Routes>
+        <Route path="/" element={<LayoutWithSidebar><Homepage /></LayoutWithSidebar>} />
+        
         <Route path="/auth" element={<AuthLayout />} />
         </Routes>
       </Router>
